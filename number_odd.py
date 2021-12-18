e8b3a4b724654215986db0471db39fad04ac0718
Your Name <ubaydullayev@samtuit.uz>
2021-12-18 23:06:47 +0500

    4-misol

--- number_odd.py
+++ number_odd.py
@@ -3,3 +3,7 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of odd digits in the variable "var_int".
+var_int = 6422
+
+print((abs((var_int % 10) % 2)-1+1) + (abs(((var_int % 100 // 10)) % 2)-1+1) + 
+      (abs(((var_int % 1000 // 10 // 10)) % 2) -1 +1) + (abs((var_int // 1000 ) % 2) - 1+1)) 

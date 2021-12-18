b9ccfea5dc08ad534c6f914b3cf34a34ed49448b
Your Name <ubaydullayev@samtuit.uz>
2021-12-18 22:31:53 +0500

    2-misol

--- is_odd.py
+++ is_odd.py
@@ -1,6 +1,5 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is odd, otherwise print 0.
-number = 4
-number = abs(number % 2 ) -1
-print(number+1)
+number = 21
+print((abs(number % 2 ) -1)+1)

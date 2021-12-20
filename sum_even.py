58ca82928348437f9f579d5181f52dab03447fc4
Your Name <ubaydullayev@samtuit.uz>
2021-12-20 15:35:26 +0500

    5-misol

--- sum_even.py
+++ sum_even.py
@@ -5,3 +5,12 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the even digits in the variable "var_int".
+
+var_int = 2222
+sum_even = 0
+a = abs(((var_int//1000)%2)-1)
+b = abs(((var_int%1000//100)%2)-1)
+c = abs(((var_int%100//10)%2)-1)
+d = abs(((var_int%10)%2)-1)
+sum_even = a+b+c+d
+print(sum_even)

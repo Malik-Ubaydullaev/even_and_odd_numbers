b5759e8835a102f3ea0d6fb9f1db916f093bef69
Your Name <ubaydullayev@samtuit.uz>
2021-12-20 15:50:43 +0500

    6-misol

--- sum_odd.py
+++ sum_odd.py
@@ -5,3 +5,16 @@
 #Create a variable "sum_even" and assign it 0.
 
 #Find the sum of the odd digits in the variable "var_int".
+
+var_int = 2111
+sum_even = 0
+a = var_int//1000%2
+b = var_int%1000//100%2
+c = var_int%100//10%2
+d = var_int%10%2
+a1 = var_int//1000
+b1 = var_int%1000//100
+c1 = var_int%100//10
+d1 = var_int%10
+sum_even = (a*a1)+(b*b1)+(c*c1)+(d*d1)
+print(sum_even)
